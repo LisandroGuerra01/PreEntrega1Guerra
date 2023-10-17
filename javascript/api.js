c4847ddc7870e150ddbc92c33de3afb671b05d85
lisandroguerra01 <lisandroguerra01@gmail.com>
2023-10-16 21:34:49 -0300

    configurando api

--- javascript/api.js
+++ javascript/api.js
@@ -1,3 +1,5 @@
+//GetPrices
+const getPrices = async () => {
     const url = 'https://crypto-and-forex-rates.p.rapidapi.com/api/getPrices';
     const options = {
         method: 'GET',
@@ -6,12 +8,32 @@
             'X-RapidAPI-Host': 'crypto-and-forex-rates.p.rapidapi.com'
         }
     };
-    
+
     try {
         const response = await fetch(url, options);
         const result = await response.json();
-        console.log(result);
+        return result;
     } catch (error) {
-        console.error(error);
+        return error;
+    }
+}
+
+//recorrer data de api y mostrar en consola
+const dataApi = document.getElementById('dataApi');
+
+const showData = async () => {
+    const data = await getPrices();
+    console.log(data);
+
+    //Array de cotizaciones a mostrar
+    let dataCotizaciones = [0, 2, 4, 5, 18, 20, 27, 50, 67, 40, 1, 8, 11, 13, 14, 10, 16, 17, 49, 117];
+    for (let i = 0; i < 20; i++) {
+        dataCotizaciones.push(data[0]);
     }
 
+    dataCotizaciones.forEach(element => {
+        dataApi.innerHTML += <span>${element.Symbol} - ${element.Price} | </span>;
+    });
+}
+
+showData();
